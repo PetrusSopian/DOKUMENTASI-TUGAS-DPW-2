9e22641c0013ef0165d62ec2730ad2461f73c9af
Petrus Sopian <petrussopian3@gmail.com>
2022-08-23 02:16:42 +0700

    tugas 2

---
+++ tugas 2/footer.php
@@ -0,0 +1,60 @@
+<link rel="stylesheet" href="style.css">
+<script src="fontawesome/js/all.min.js"></script>
+<footer>
+      <div class="main-content">
+        <div class="left box">
+          <h2>Tentang Kami</h2>
+          <div class="content">
+            <p>Toko kami menjual berbagai bahan bangunan lengkap apapun yang anda cari ada di toko kami kunjungilah toko kami yang terletak dipinggir jalan  </p>
+            <div class="social">
+              <a href="https://facebook.com/coding.np"><span class="fab fa-facebook-f"></span></a>
+              <a href="#"><span class="fab fa-twitter"></span></a>
+              <a href="https://instagram.com/coding.np"><span class="fab fa-instagram"></span></a>
+              <a href="https://youtube.com/c/codingnepal"><span class="fab fa-youtube"></span></a>
+            </div>
+          </div>
+        </div>
+        <div class="center box">
+          <h2>Alamat</h2>
+          <div class="content">
+            <div class="place">
+              <span class="fas fa-map-marker-alt"></span>
+              <span class="text">sukaharja,ketapang</span>
+            </div>
+            <div class="phone">
+              <span class="fas fa-phone-alt"></span>
+              <span class="text">+089-765432100</span>
+            </div>
+            <div class="email">
+              <span class="fas fa-envelope"></span>
+              <span class="text">petrussopian3@gmail.com</span>
+            </div>
+          </div>
+        </div>
+        <div class="right box">
+          <h2>Kontak Kami</h2>
+          <div class="content">
+            <form action="#">
+              <div class="email">
+                <div class="text">Email *</div>
+                <input type="email" required>
+              </div>
+              <div class="msg">
+                <div class="text">Pesan *</div>
+                <textarea id=".form.msg" rows="2" cols="25" required></textarea><br>
+
+              <div class="btn">
+                <button class="btn btn-success" type="submit">Kirim</button>
+              </div>
+            </form>
+          </div>
+          <div class="bottom">
+	        <center>
+	          <span class="credit">Created By <a href="https://youtube.com/ppnurulummah">petrus sopian</a> </span>
+	          <span class="far fa-copyright"></span> 2022 All rights reserved.
+	        </center>
+	      </div>
+</footer>
+
+	      
+     
