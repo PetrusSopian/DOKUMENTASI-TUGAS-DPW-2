24d107a0a54f4947598ff2eda826a0e134e7d23d
Petrus Sopian <petrussopian3@gmail.com>
2022-08-23 02:19:12 +0700

    tugas 7

---
+++ tugas 7/kategoriproduk.php
@@ -0,0 +1,96 @@
+<style type="text/css">
+	.col-md-3{
+		position: relative;
+		margin:0 auto;
+		overflow: hidden;
+	}
+	.tumbnail{
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+	.thumbnail img{
+		padding: 10px;
+		-webkit-transition:0.4 ease;
+		transition: 0.4 ease;
+	}
+	.col-md-3:hover .thumbnail img{
+		-webkit-transform:scale(1.36);
+		transform: scale(1.36);
+	}
+	
+</style>
+<?php 
+session_start();
+include 'koneksi.php';
+$kategori = $_GET["id"];
+
+
+$semuadata=array();
+$ambil=$koneksi->query("SELECT*FROM produk WHERE id_kategori LIKE '%$kategori%'");
+WHILE($pecah=$ambil->fetch_assoc())
+{
+	$semuadata[]=$pecah;
+}
+?>
+<?php
+$datakategori=array();
+$ambil= $koneksi->query("SELECT * FROM kategori");
+while($tiap=$ambil->fetch_assoc())
+{
+	$datakategori[]=$tiap;
+}
+?>
+<?php $am= $koneksi->query("SELECT * FROM kategori where id_kategori='$kategori'");
+$pe=$am->fetch_assoc()
+?>
+<?php include 'menu.php'; ?><br><br><br><br><br>
+<?php include 'buttonup.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kategori</title>
+	<link rel="stylesheet" type="text/css" href="admin/assets/css/bootstrap.css">
+</head>
+<body>
+	<div class="container">
+		<form action="pencarian.php" method="get" class="navbar-form navbar-right">
+			<input type="text" class="form-control" name="keyword" placeholder="Cari Produk">
+			<button class="btn btn-primary"><i class="fas fa-search"></i></button>
+		</form>
+		<form action="pencarian.php" method="get" class="navbar-form navbar-right">
+				<input type="text" class="form-control" name="keyword" placeholder="filter produk">
+				 <td> <input type="submit" class="btn btn-primary" name="filter" value="filter"></td>
+		</form>
+		
+		
+		<h3> Kategori : <?php echo $pe["nama_kategori"] ?></h3>
+
+		<?php if (empty($semuadata)): ?>
+			<div class="alert alert-danger">Produk <strong><?php echo  $pe["nama_kategori"]?></strong> Kosong</div>
+			
+		<?php endif ?>
+
+		<div class="row">
+			
+
+			<?php foreach ($semuadata as $key => $value): ?>
+				
+			
+			<div class="col-md-3" style=" padding:  5px;">
+				<div class="thumbnail" style="border: 3px solid black;">
+					<img src="foto_produk/<?php echo $value['foto_produk'] ?>"class="img-responsive" width="100" alt="">
+					<div class="caption">
+						<h3><?php echo $value['nama_produk'] ?></h3>
+						<h5>Rp <?php echo number_format($value['harga_produk']) ?></h5>
+						<a href="beli.php?id=<?php echo $value['id_produk']; ?>" class="btn btn-primary"> Beli</a>
+						<a href="detail.php?id=<?php echo $value['id_produk']; ?>" class="btn btn-default">Detail</a>
+
+					</div>
+				</div>
+			</div>
+			<?php endforeach ?>
+		</div>
+	</div>
+</body>
+</html>
---
+++ tugas 7/menu.php
@@ -0,0 +1,141 @@
+<style type="text/css">
+  nav{
+      position: fixed;
+    width: 100%;
+    z-index: 99999;
+  background: blue;
+  padding: 1px 20px;
+}
+nav ul{
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+nav ul li{
+  padding: 15px 0;
+  cursor: pointer;
+}
+nav ul li.items{
+  position: relative;
+  width: auto;
+  margin: 0 16px;
+  text-align: center;
+  order: 3;
+}
+nav ul li.items:after{
+  position: absolute;
+  content: '';
+  left: 0;
+  bottom: 5px;
+  height: 2px;
+  width: 100%;
+  background: #33ffff;
+  opacity: 0;
+  transition: all 0.2s linear;
+}
+nav ul li.items:hover:after{
+  opacity: 1;
+  bottom: 8px;
+}
+nav ul li.logo{
+  flex: 1;
+  color: white;
+  font-size: 23px;
+  font-weight: 600;
+  cursor: default;
+  user-select: none;
+}
+nav ul li a{
+  color: white;
+  font-size: 18px;
+  text-decoration: none;
+  transition: .4s;
+}
+nav ul li:hover a{
+  color: cyan;
+}
+nav ul li i{
+  font-size: 23px;
+}
+nav ul li.btn1{
+
+  display: none;
+}
+
+nav ul li.btn1.hide i:before{
+  content: '\f00d';
+}
+@media all and (max-width: 900px){
+  nav{
+    padding: 5px 30px;
+
+  }
+  nav ul li.items{
+    width: 100%;
+    display: none;
+
+  }
+  nav ul li.items.show{
+    display: block;
+
+  }
+  nav ul li.btn1{
+    display: block;
+
+  }
+  nav ul li.items:hover{
+    border-radius: 5px;
+    box-shadow: inset 0 0 5px #33ffff,
+                inset 0 0 10px #66ffff;
+  }
+  nav ul li.items:hover:after{
+    opacity: 0;
+  }
+}
+</style>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="fontawesome/js/all.min.js"></script>
+ <script src="admin/assets/js/jquery-1.10.2.js"></script>
+	<nav>
+      <ul>
+<li class="logo">TOKO BANGUNAN SUPER</li>
+<li class="items"><a href="index.php"><i class="fas fa-home"></i> Home</a></li>
+<form method="get" class="navbar-form navbar-right">
+			<select class="form-control" name="kategori" onchange="document.location.href= this.options[this.selectedIndex].value;">
+	 			<option value="">Pilih Kategori</option>
+	 			<?php foreach ($datakategori as $key => $value): ?>
+	 			<option value="kategori.php?id=<?php echo $value["id_kategori"] ?>" value="<?php echo $value["id_kategori"] ?>" ><?php echo $value["nama_kategori"] ?> </option>
+	 			<?php endforeach ?>
+ 			</select>
+		</form><br>
+<li class="items"><a href="kategoriproduk.php"><i class="fa fa-tags"></i> Produk Utama</a></li>
+<?php 
+include 'koneksi.php';
+if (isset($_SESSION["pelanggan"])):?>
+	<li class="items"><a href="logout.php">Logout</a></li>
+	<li class="items"><a href="riwayat.php">Riwayat Pembelian</a></li>
+  <?php 
+$id_pelanggan= $_SESSION["pelanggan"]['id_pelanggan'];
+$ambil= $koneksi->query("SELECT *FROM pelanggan WHERE id_pelanggan='$id_pelanggan'");
+$pecah=$ambil->fetch_assoc();?>
+	<li class="items"><a href="profil.php"><img src="fotoprofil/<?php echo $pecah["fotoprofil"] ?>" width="22px" style="border-radius: 15px; -moz-border-radius:15px; border: 2px solid crimson;"></a></li>
+<?php else: ?>
+	<li class="items"><a href="login.php">Login</a></li>
+	<li class="items"><a href="daftar.php">Daftar</a></li>
+<?php endif ?> 
+<li class="btn1"><a href="#"><i class="fas fa-bars"></i></a></li>
+</ul>
+</nav>
+    <script>
+      $(document).ready(function(){
+        $('.btn1').click(function(){
+          $('.logo').toggleClass("show");
+          $('.btn1').toggleClass("show");
+          $('.items').toggleClass("show");
+          $('ul li').toggleClass("hide");
+        });
+      });
+    </script>
